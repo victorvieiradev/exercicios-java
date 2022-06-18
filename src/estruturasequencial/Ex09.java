d1ab2b5b477ecf538f221c8e94c1542a9a190ba0
victorvieiradev <contatovictorvieira@outlook.com>
2022-06-18 14:41:57 -0300

    atualizando algumas informações da lista sequêncial

--- src/estruturasequencial/Ex09.java
+++ src/estruturasequencial/Ex09.java
@@ -1,2 +1,16 @@
-package estruturasequencial;public class Ex09 {
+package estruturasequencial;
+
+import java.util.Scanner;
+
+//9. Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+//C = 5 * ((F-32) / 9).
+public class Ex09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a temperatura em graus Fahrenheit: ");
+        double F = input.nextDouble();
+        double C = 5 * ((F-32) / 9);
+        System.out.println("Determinado o grau informado convertendo ele o resultado será " + C + " graus celcius.");
+        input.close();
+    }
 }
--- src/estruturasequencial/Ex10.java
+++ src/estruturasequencial/Ex10.java
@@ -1,2 +1,14 @@
-package estruturasequencial;public class Ex10 {
+package estruturasequencial;
+
+import java.util.Scanner;
+//10. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe a temperatura em graus celcius:");
+        double c = input.nextDouble();
+        double f = (c * 9/5) + 32;
+        System.out.println("determinado grau informado convertendo ele o  resultado será " + f + " graus Fahrenheit");
+        input.close();
+    }
 }
