d58438c30592e4b611649bfaa629ba6ea3ae4908
victorvieiradev <contatovictorvieira@outlook.com>
2022-06-24 13:09:57 -0300

    feito dois exercícios da lista 03b

---
+++ src/catalisa/lista03b/ContandoAte100.java
@@ -0,0 +1,14 @@
+package catalisa.lista03b;
+
+public class ContandoAte100 {
+    public static void main(String[] args) {
+        int c = 1;
+        while ( c <= 100){
+            System.out.println(c);
+            c++;
+        }
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/catalisa/lista03b/SomandoNumerosInteiros.java
@@ -0,0 +1,26 @@
+package catalisa.lista03b;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SomandoNumerosInteiros {
+    public static void main(String[] args) {
+        int soma = 0;
+        double media = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe a quantidade de valores para somar e calcular a média: ");
+        int quantidade = input.nextInt();
+        int[] numeros = new  int[quantidade];
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println((i+1) + "º valor");
+            numeros[i] = input.nextInt();
+        }
+        input.close();
+        for (int numero : numeros){
+            soma += numero;
+        }
+        media = soma / quantidade;
+        System.out.println("a soma dos valores inseridos é: " + soma + " já a média é " + media);
+    }
+}
