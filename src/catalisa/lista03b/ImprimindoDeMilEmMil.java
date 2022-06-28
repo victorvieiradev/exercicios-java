25c6a1010c3a149c3f1e3a7923bc173521dbc6c4
victorvieiradev <contatovictorvieira@outlook.com>
2022-06-28 13:53:05 -0300

    corrigido

--- src/catalisa/lista03b/ImprimindoDeMilEmMil.java
+++ src/catalisa/lista03b/ImprimindoDeMilEmMil.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ImprimindoDeMilEmMil {
     public static void main(String[] args) {
         int i = 0;
-        while (i < 100000){
+        while (i <= 100000){
             System.out.println("contando... " + i);
             i += 1000;
         }
