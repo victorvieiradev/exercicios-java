2e5b3ab7f0f55e16ba1ea3635fe8e809196d079d
victorvieiradev <contatovictorvieira@outlook.com>
2022-07-07 10:30:06 -0300

    ajuste na identação

--- src/catalisa/lista05/parte01/Bola.java
+++ src/catalisa/lista05/parte01/Bola.java
@@ -13,10 +13,8 @@ public class Bola {
     public String getCor() {
         return cor;
     }
-
     public void setCor(String cor) {
         this.cor = cor;
     }
-
 
 }
--- src/catalisa/lista05/parte01/BolaMain.java
+++ src/catalisa/lista05/parte01/BolaMain.java
@@ -2,6 +2,7 @@ package catalisa.lista05.parte01;
 
 public class BolaMain {
     public static void main(String[] args) {
+        
         Bola b1 = new Bola("vermelha", "borracha", 90);
         b1.setCor("Azul");
         System.out.println("Cor: " + b1.getCor());
