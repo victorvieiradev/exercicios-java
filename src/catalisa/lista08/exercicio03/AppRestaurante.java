7b12567b83b1ef0fb80a8ce945baba57ada372e4
victorvieiradev <contatovictorvieira@outlook.com>
2022-07-16 20:00:12 -0300

    corrigido erros de pt-br

--- src/catalisa/lista08/exercicio03/AppRestaurante.java
+++ src/catalisa/lista08/exercicio03/AppRestaurante.java
@@ -16,9 +16,9 @@ public class AppRestaurante {
                 System.out.println("Qual a quantidade de pratos você deseja adicionar: ");
                 int qtd = input.nextInt();
                 for (int i = 1; i <= qtd; i++){
-                    System.out.println(i + "º Nome do primeiro prato: ");
+                    System.out.println(i + "º Nome do prato: ");
                     String nome = input.next();
-                    System.out.println("qqual é a quantidade de ingredientes para o prato " + nome);
+                    System.out.println("qual é a quantidade de ingredientes para o prato " + nome);
                     int quantidadeIngredientes = input.nextInt();
                     List<String> listaIngredientes = new ArrayList<>();
                     for (int j = 1; j <= quantidadeIngredientes; j++){
@@ -30,7 +30,7 @@ public class AppRestaurante {
             }else if (op==2){
                 restaurante.listar();
             }else if (op==3){
-                System.out.println("Digite corretamente o nome do plato para excluir");
+                System.out.println("Digite corretamente o nome do prato para excluir");
                 String nome = input.next();
                 restaurante.excluir(nome);
             }else if (op==4){
